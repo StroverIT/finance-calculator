4eb73836bf5d1565c0cfc7476d5b948c8bc6ab7a
StR0vErBG <xcreaw1@abv.bg>
2023-02-26 20:41:50 +0200

    changing to fixed position on background

--- pages/_app.js
+++ pages/_app.js
@@ -7,7 +7,7 @@ function MyApp({ Component, pageProps }) {
     <>
       <Component {...pageProps} />
       <ToastContainer />
-      <div className="absolute  top-0 left-0 -z-10 h-screen w-screen ">
+      <div className="fixed  top-0 left-0 -z-10 h-screen w-screen ">
         <div className="absolute bg-index h-screen w-[100vw] top-0 left-0 z-10"></div>
 
         <div className="h-screen w-screen relative bg-index">
