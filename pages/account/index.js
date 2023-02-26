fc69745746d944cc97576b9b7e24d953604c0c7b
StR0vErBG <xcreaw1@abv.bg>
2023-02-26 20:42:31 +0200

    fixing spacing

--- pages/account/index.js
+++ pages/account/index.js
@@ -1,46 +1,58 @@
 import { getSession } from "next-auth/react";
+import Image from "next/image";
 import { useRouter } from "next/router";
+import { BsCalendarDay, BsCalendarMonth } from "react-icons/bs";
+import { FcDebt } from "react-icons/fc";
 import LogoutBtn from "../../components/LogoutBtn";
 const FinanceInput = ({ data, session }) => {
   const router = useRouter();
   return (
     <main className="  h-screen my-auto flex-center">
-      <div className="py-10 bg-white flex-center px-20 flex-col">
-        <LogoutBtn />
+      <div className="py-10 bg-white flex-center px-20 flex-col ">
+        <LogoutBtn className=" mb-2" />
         <div className="mb-10 text-6xl font-bold text-blue">
           Finance Calculator
         </div>
-        <div className="grid grid-cols-3 gap-16 text-lg">
+        <div className="grid grid-cols-3 gap-16 text-2xl mt-10">
           <button
-            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg  w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            className="text-white  flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg  w-full sm:w-auto px-7 py-4 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 justify-center"
             onClick={() => router.push("/account/monthExpense")}
           >
+            <div className="relative h-14 w-14 mr-2">
+              <Image src="/icons/month.png" alt="month" fill />
+            </div>
             Месечни сметки
           </button>
           <button
-            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg  w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            className="text-white flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg  w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 justify-center"
             onClick={() => router.push("/account/dayExpense")}
           >
+            <div className="relative h-12 w-12 mr-2">
+              <Image src="/icons/day-and-night.png" alt="month" fill />
+            </div>
             Дневни сметки
           </button>
           <button
-            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg  w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-            onClick={() => router.push("/account/dept")}
+            className="text-white flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg  w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800  justify-center"
+            onClick={() => router.push("/account/debt")}
           >
+            <span className="pr-1 text-4xl">
+              <FcDebt />
+            </span>
             Дължими суми
           </button>
-          <button
+          {/* <button
             className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg  w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             onClick={() => router.push("/")}
           >
             Важни дати
-          </button>
-          <button
+          </button> */}
+          {/* <button
             onClick={() => router.push("/")}
             className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg  w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
             Планове
-          </button>
+          </button> */}
         </div>
       </div>
     </main>
