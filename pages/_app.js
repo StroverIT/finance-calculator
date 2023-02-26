f60423d799449ceba1d8c40b49289fb9c1072b05
StR0vErBG <xcreaw1@abv.bg>
2023-02-26 18:52:21 +0200

    adding background to all pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,26 @@
+import Image from "next/image";
+import { ToastContainer } from "react-toastify";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <ToastContainer />
+      <div className="absolute  top-0 left-0 -z-10 h-screen w-screen ">
+        <div className="absolute bg-index h-screen w-[100vw] top-0 left-0 z-10"></div>
+
+        <div className="h-screen w-screen relative bg-index">
+          <Image
+            src="/background3.jpg"
+            alt="background image"
+            fill
+            className="object-"
+          />
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default MyApp;
