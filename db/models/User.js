21978ad430dd32f30a2c0505b30237efda6992c3
StR0vErBG <xcreaw1@abv.bg>
2023-02-26 18:53:30 +0200

    creating user comp

--- db/models/User.js
+++ db/models/User.js
@@ -1,8 +1,24 @@
 import { Schema, model, models } from "mongoose";
 
 const userSchema = new Schema({
-  name: String,
-  finance: { type: Schema.Types.ObjectId, ref: "Finance" },
+  name: {
+    type: String,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  isSocialMedia: Boolean,
+  monthlyFinance: { type: Schema.Types.ObjectId, ref: "MonthlyFinance" },
+  dailyFinance: { type: Schema.Types.ObjectId, ref: "DailyFinance" },
+  dept: { type: Schema.Types.ObjectId, ref: "Dept" },
+  importantDates: { type: Schema.Types.ObjectId, ref: "ImportantDates" },
+  plans: { type: Schema.Types.ObjectId, ref: "Plans" },
 });
 
 const User = models.User || model("User", userSchema);
