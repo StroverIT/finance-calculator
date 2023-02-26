772477c165b1b7e172db8ade0cb9efd985fab1eb
StR0vErBG <xcreaw1@abv.bg>
2023-02-26 23:29:45 +0200

    ready ui

---
+++ pages/account/report.js
@@ -0,0 +1,118 @@
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
+import FinanceCalc from "../../components/FinanceCalc";
+import { connectMongo } from "../../db/connectDb";
+import User from "../../db/models/User";
+import { BsBoxArrowInLeft } from "react-icons/bs";
+import { getSession } from "next-auth/react";
+import Head from "next/head";
+
+import DatePickerComp from "../../components/DatePickerComp";
+
+const FinanceInput = ({ data, session }) => {
+  const [totalSum, setTotalSum] = useState(0);
+
+  const [dateInput, setDateInput] = useState("");
+
+  const financeData = data.dailyFinance;
+  const typeFinance = "daily";
+
+  useEffect(() => {
+    const totalExpense = financeData.expense.totalSums.reduce(
+      (x, y) => x + Number(y.price),
+      0
+    );
+    setTotalSum(totalExpense);
+  }, [data]);
+
+  return (
+    <>
+      <Head>
+        <title>Daily expenses</title>
+        <meta
+          name="description"
+          content="Your personal finance statement app"
+        />
+      </Head>
+      <main className="flex-col h-screen my-auto flex-center">
+        <div className="flex-col p-10 bg-white rounded-md shadow-2xl flex-center">
+          <div className="flex items-center text-sm">
+            <div className="mr-1 text-xl">
+              <BsBoxArrowInLeft />
+            </div>
+            <Link href="/account" className="text-lg">
+              Назад
+            </Link>
+          </div>
+          <div className="mb-10 text-5xl font-bold text-blue">Дневен отчет</div>
+          <DatePickerComp setDateInput={setDateInput} />
+
+          <div className="grid justify-center mt-10 gap-x-28">
+            <FinanceCalc
+              text={financeData.expense.text}
+              totalSums={financeData.expense.totalSums}
+              type="expense"
+              session={session}
+              typeFinance={typeFinance}
+            />
+          </div>
+          <div className="mt-10 text-2xl text-center">
+            Похарчени пари за деня:{" "}
+            <span className="pl-1 font-semibold">{totalSum}</span>
+          </div>
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default FinanceInput;
+export async function getServerSideProps(context) {
+  const { query } = context;
+  const session = await getSession({ req: context.req });
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+  let data = {};
+  try {
+    await connectMongo();
+
+    const user = await User.findOne({ email: session.user.email }).populate(
+      "dailyFinance"
+    );
+    data = user;
+  } catch (e) {
+    console.log(e);
+  }
+  return {
+    props: {
+      data: JSON.parse(JSON.stringify(data)),
+      session: JSON.parse(JSON.stringify(session)),
+    }, // will be passed to the page component as props
+  };
+}
+// const data = {
+//   income: {
+//     text: "Приход:",
+//     totalSums: [
+//       {
+//         price: 100,
+//         reason: "No reason",
+//       },
+//     ],
+//   },
+//   expense: {
+//     text: "Разход:",
+//     totalSums: [
+//       {
+//         price: 20,
+//         reason: "No reason",
+//       },
+//     ],
+//   },
+// };
