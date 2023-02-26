d81c6d1e77ad784e008203aee582ec07fa80cd3e
StR0vErBG <xcreaw1@abv.bg>
2023-02-26 18:47:32 +0200

    Almost ready finance calc comp

--- components/FinanceCalc.js
+++ components/FinanceCalc.js
@@ -1,24 +1,91 @@
-import React, { useState } from "react";
+import { useRouter } from "next/router";
+import React, { useEffect, useState } from "react";
 import ColorInput from "./ColorInput";
+import { toastSuccess } from "./notifications/Toast";
+import { HiX } from "react-icons/hi";
 
-const FinanceCalc = ({ text, totalSums }) => {
-  console.log(text == "Разход:");
-  const [input, setInput] = useState("");
+import { getSession, signOut } from "next-auth/react";
+
+const FinanceCalc = ({ text, totalSums, type, session, typeFinance }) => {
+  const router = useRouter();
+  const [priceInput, setPriceInput] = useState("");
+  const [reasonInput, setReasonInput] = useState("");
+
+  const submitHandler = async () => {
+    const res = await fetch("/api/finance", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        type,
+        price: priceInput,
+        reason: reasonInput,
+        typeFinance,
+      }),
+    });
+    const resData = await res.json();
+    if (resData.message) {
+      toastSuccess(resData.message);
+    }
+    setPriceInput("");
+    setReasonInput("");
+    router.replace(router.asPath);
+  };
+  const removeHandler = async (stateId) => {
+    const res = await fetch("/api/removeFinance", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        type,
+        stateId,
+        typeFinance,
+      }),
+    });
+    const resData = await res.json();
+    if (resData.message) {
+      toastSuccess(resData.message);
+    }
+    router.replace(router.asPath);
+  };
   return (
     <div>
       <h1 className="mb-2 text-xl font-bold">{text} </h1>
 
-      <ColorInput
-        labelName="Сума:"
-        btnName="Добави"
-        input={input}
-        setInput={setInput}
-      />
-      <div className={`mt-10 ${text == "Разход:" && "text-red-500"}`}>
-        {totalSums.map((sum) => {
+      <div className="grid grid-cols-2 gap-x-5">
+        <ColorInput
+          labelName="Сума:"
+          input={priceInput}
+          setInput={(e) => setPriceInput(e.target.value)}
+          isBtn={false}
+        />
+        <ColorInput
+          labelName="Причина:"
+          input={reasonInput}
+          setInput={(e) => setReasonInput(e.target.value)}
+          isBtn={false}
+        />
+        <button
+          type="submit"
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          onClick={submitHandler}
+        >
+          Добави
+        </button>
+      </div>
+      <div className={`mt-10 `}>
+        {totalSums.map((sum, index) => {
           return (
-            <div key={sum.reason}>
+            <div key={sum._id} className="flex items-center text-lg">
               {sum.price} - {sum.reason}
+              <div
+                className="mt-1 ml-auto text-xl text-red-500 cursor-pointer "
+                onClick={() => removeHandler(sum._id)}
+              >
+                <HiX />
+              </div>
             </div>
           );
         })}
