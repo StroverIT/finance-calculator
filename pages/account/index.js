c850eb08daba68352ae89a1900740e08c48b1c78
StR0vErBG <xcreaw1@abv.bg>
2023-02-26 23:30:15 +0200

    adding daily report

--- pages/account/index.js
+++ pages/account/index.js
@@ -12,18 +12,18 @@ const FinanceInput = ({ data, session }) => {
       <Head>
         <title>Your personal finance</title>
       </Head>
-      <main className="  h-screen my-auto flex-center">
-        <div className="py-10 bg-white flex-center px-20 flex-col ">
-          <LogoutBtn className=" mb-2" />
-          <div className="mb-10 text-2xl md:text-6xl font-bold text-blue">
+      <main className="h-screen my-auto flex-center">
+        <div className="flex-col px-20 py-10 bg-white flex-center ">
+          <LogoutBtn className="mb-2 " />
+          <div className="mb-10 text-2xl font-bold md:text-6xl text-blue">
             Finance Calculator
           </div>
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-16 gap-y-5 text-md sm:text-2xl mt-10">
+          <div className="grid grid-cols-1 mt-10 md:grid-cols-2 lg:grid-cols-3 gap-x-16 gap-y-5 text-md sm:text-2xl">
             <button
-              className="text-white  flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg  w-full sm:w-auto px-7 py-4 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 justify-center"
+              className="flex items-center justify-center w-full py-4 font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 sm:w-auto px-7 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
               onClick={() => router.push("/account/monthExpense")}
             >
-              <div className="relative h-10 w-10 sm:h-14 sm:w-14 mr-2">
+              <div className="relative w-10 h-10 mr-2 sm:h-14 sm:w-14">
                 <Image src="/icons/month.png" alt="month" fill />
               </div>
               Месечни сметки
@@ -32,7 +32,7 @@ const FinanceInput = ({ data, session }) => {
               className="text-white flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg  w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 justify-center"
               onClick={() => router.push("/account/dayExpense")}
             >
-              <div className="relative h-8 w-8 sm:h-14 sm:w-14 mr-2">
+              <div className="relative w-8 h-8 mr-2 sm:h-14 sm:w-14">
                 <Image src="/icons/day-and-night.png" alt="month" fill />
               </div>
               Дневни сметки
@@ -45,6 +45,15 @@ const FinanceInput = ({ data, session }) => {
                 <FcDebt />
               </span>
               Дължими суми
+            </button>
+            <button
+              className="text-white flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg  w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 justify-center"
+              onClick={() => router.push("/account/report")}
+            >
+              <div className="relative w-8 h-8 mr-2 sm:h-14 sm:w-14">
+                <Image src="/icons/business-report.png" alt="month" fill />
+              </div>
+              Дневен отчет
             </button>
             {/* <button
               className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg  w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
